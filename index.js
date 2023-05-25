b45204b72eb99348d1987a7d0988e55d86dc633c
thejjwang <jonathanjacobwang@gmail.com>
2023-05-24 23:21:19 -0700

    mvp

--- index.js
+++ index.js
@@ -8,34 +8,32 @@ let ul = document.querySelector('ul');
 newArr = []
 
 button.addEventListener('click', myFunction);
+
+// }
+// input.addEventListener('keydown', function(event) {
+//     if (event.key === 'Enter') {
+//       myFunction();
+//       renderTasks();
+//       input.value = "";
+//     }
+// });
 function myFunction(){
-
+    newArr.push(input.value);
+    console.log(newArr)
+    renderTasks();
 }
-input.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      myFunction();
-      renderTasks();
-      input.value = "";
-    }
-  });
-  function myFunction(){
-      newArr.push(input.value);
-      console.log(newArr)
-  }
-  function renderTasks(){
-      ul.innerHTML = "";
-      newArr.forEach((listItem) => {
-      let newLi = document.createElement('li');
-      let newButton = document.createElement('button');
-      newButton.addEventListener('click', deleteItem);
-      newButton.textContent = "delete";
-      newLi.textContent = listItem;
-      newLi.addEventListener('click', deleteItem);
-      ul.appendChild(newLi);
-      newLi.appendChild(newButton);});
-      
+function renderTasks(){
+    ul.innerHTML = "";
+    newArr.forEach((listItem) => {
+    let newLi = document.createElement('li');
+    let newButton = document.createElement('button');
+    newButton.addEventListener('click', deleteItem);
+    newButton.textContent = "delete";
+    newLi.textContent = listItem;
+    newLi.addEventListener('click', deleteItem);
+    ul.appendChild(newLi);
+    newLi.appendChild(newButton);}); 
 }
-
 function deleteItem(){
     let itemText = this.textContent;
     let index = newArr.indexOf(itemText);
